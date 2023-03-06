fe0b21ac311045ab2bd08434b12737be206c8e57
MariuszBaran <mariusz777@gmail.com>
2023-03-06 13:14:59 +0100

    pop

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,23 @@
-const input = ( document.querySelector("input").value )
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
 
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(box);
+    size += 10;
+}
+}
 
-
-
-
-
-
-
-
-
-
-
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -18,3 +25,5 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+createBtn.addEventListener("click", () => createBoxes(input.value));
+destroyBtn.addEventListener("click", destroyBoxes);
